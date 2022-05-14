10f5d8f75e4c081f9cc59e1ef46330edaaf8ca78
vmaevski <vmaevski@mail.ru>
2022-05-14 17:34:45 +0700

    Completed task 2

---
+++ 220514_Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+int ThirdDigit(int a)
+{
+    if (a < 100)
+    {
+        return -1;
+    }
+    else
+    {
+        
+    }
+
+
+}
+
+int RandomNumber = new Random().Next(0, 100000);
+Console.WriteLine($"{RandomNumber} -> {ThirdDigit(RandomNumber)}");
