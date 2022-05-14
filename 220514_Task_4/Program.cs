74ed3a9c382a51d9552d12c9c429e0100583b27e
vmaevski <vmaevski@mail.ru>
2022-05-14 18:11:54 +0700

    Completed task 4

---
+++ 220514_Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и 
+// проверяет, является ли этот день выходным.
+// Будем считать, что суббота - день с номером 6, воскресенье - 7.
+
+void DayOff(int a)
+{
+    if (a == 6 | a == 7)
+    {
+//        return ("YES!!!");
+        Console.WriteLine($"{a} -> YES!!!");
+    }
+    else
+    {
+//        return ("No(");
+        Console.WriteLine($"{a} -> No(");
+    }
+}
+
+int RandomNumber = new Random().Next(1, 8);
+DayOff(RandomNumber);
+// Console.WriteLine($"{RandomNumber} -> {DayOff(RandomNumber)}");
