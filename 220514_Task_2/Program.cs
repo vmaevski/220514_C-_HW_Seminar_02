4c995be942e2c5979f8852b639a69701cc9cf092
vmaevski <vmaevski@mail.ru>
2022-05-14 17:09:50 +0700

    Completed task 2

---
+++ 220514_Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+int DelSecDigit(int a)
+{
+    return a / 100 * 10 + a % 10;
+}
+
+int RandomNumber = new Random().Next(100, 1000);
+Console.WriteLine($"{RandomNumber} -> {DelSecDigit(RandomNumber)}");
